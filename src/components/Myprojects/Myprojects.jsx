70881d17ced6ba89875ec707f708450d980a3364
carreguz13 <carreguz13@gmail.com>
2023-02-05 21:52:19 -0700

    js toggle done

--- src/components/Myprojects/Myprojects.jsx
+++ src/components/Myprojects/Myprojects.jsx
@@ -2,6 +2,13 @@ import React from 'react'
 import "./Myprojects.css"
 import portimg from "../../assets/port.PNG"
 import libreriaimg from "../../assets/libreria.PNG"
+import flipperimg from "../../assets/flipper.PNG"
+import counterimg from "../../assets/counter.PNG"
+import reviewsimg from "../../assets/reviews.PNG"
+import navbarimg from "../../assets/navbar.PNG"
+
+
+
 
 
 //functions for every buton and make toggle possible 
@@ -28,40 +35,50 @@ function Myprojects() {
       <h1>My projects</h1>
 
       <h2>A selection of my projects.</h2>
-
+<div>
       <button className='react-btn' onClick={react}>react</button>
       <button className='js-btn' onClick={javaScript}>JavaScript</button>
-      
+      </div>
      
 
      <section className='react-section'>
 <div className='p1'>
-  <a href="https://carlosarreola.me/" target="_blank"><img src={portimg} alt="" /></a>
+  <a href="https://carlosarreola.me/" target="_blank" rel='noreferrer'><img src={portimg} alt="" /></a>
   <h3>Portfolio</h3>
-  <p>This is my portfolio web when I show some projects and a share a little bit about me.</p>
+  <p>Portfolio web site, when I show some projects and share a little bit about me</p>
 </div>
 <div className='p2'>
-<a href="https://carreguz13.github.io/Libreria/" target="_blank"><img src={libreriaimg} alt="" /></a>
+<a href="https://carreguz13.github.io/Libreria/" target="_blank" rel='noreferrer'><img src={libreriaimg} alt="" /></a>
   <h3>Library</h3>
-  <p>Application to save, edit and delete book information.</p>
+  <p>Application to save, edit and delete books information</p>
 </div>
-<div className='p3'>
-<img src={portimg} alt="" />
-  <h3>Portfolio</h3>
-  <p>This is my portfolio web when I show some projects and a share a little bit about me.</p>
-</div>
-<div className='p4'>
-<img src={portimg} alt="" />
-  <h3>Portfolio</h3>
-  <p>This is my portfolio web when I show some projects and a share a little bit about me.</p>
-</div>
-     </section>
-
-     <section className='js-section'>
-<div className='p1'></div>
-<div className='p2'></div>
 <div className='p3'></div>
 <div className='p4'></div>
+     </section>
+
+
+
+     <section className='js-section'>
+<div className='p5'>
+  <a href="https://carreguz13.github.io/color-flipper/" target="_blank" rel='noreferrer'><img src={flipperimg} alt="" /></a>
+  <h3>Color flipper</h3>
+  <p>Change background color randomly by clicking a button</p>
+</div>
+<div className='p6'>
+<a href="https://carreguz13.github.io/counter/" target="_blank" rel='noreferrer'><img src={counterimg} alt="" /></a>
+  <h3>Counter</h3>
+  <p>Increase, decrease and reset</p>
+</div>
+<div className='p7'>
+<a href="https://carreguz13.github.io/reviews/" target="_blank" rel='noreferrer'><img src={reviewsimg} alt="" /></a>
+  <h3>Reviews</h3>
+  <p>Navigate into multiple reviews in order or randomly</p>
+</div>
+<div className='p8'>
+<a href="https://carreguz13.github.io/navbar/" target="_blank" rel='noreferrer'><img src={navbarimg} alt="" /></a>
+  <h3>Navbar</h3>
+  <p>Navbar component with responsive toggle function</p>
+</div>
      </section>
      
      </div>
